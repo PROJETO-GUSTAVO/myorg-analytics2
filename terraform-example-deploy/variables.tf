2d21d87dba0add19f73ed006a0e26557e7cef335
Gustavo Ramos Lima <45151632+glima91@users.noreply.github.com>
2024-11-02 16:33:54 -0300

    ajuste terraform

--- terraform-example-deploy/variables.tf
+++ terraform-example-deploy/variables.tf
@@ -1,3 +1,3 @@
-# variable "resource_group_name" {
-#   type = string
-# }
+variable "resource_group_name" {
+  type = string
+}
--- terraform-example-deploy/virtual_machine.tf
+++ terraform-example-deploy/virtual_machine.tf
@@ -1,6 +1,6 @@
-# data "azurerm_resource_group" "example" {
-#   name = var.resource_group_name
-# }
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
+}
 
 # resource "azurerm_virtual_network" "example" {
 #   name                = "example-network"
